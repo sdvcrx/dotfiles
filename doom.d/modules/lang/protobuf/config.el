7f9d4ee1594a74f151f8735b2a88c2a74f215d43
sdvcrx <memory.silentvoyage@gmail.com>
2020-01-27 22:13:38 +0800

    emacs: remove protobuf module

--- doom.d/modules/lang/protobuf/config.el
+++
@@ -1,9 +0,0 @@
-;;; lang/proto/config.el -*- lexical-binding: t; -*-
-
-(use-package! protobuf-mode
-  :mode "\\.proto$"
-  :init
-  ;; copy from spacemacs
-  (setq-hook! 'protobuf-mode-hook
-   imenu-generic-expression '((nil "^[[:space:]]*\\(message\\|service\\|enum\\)[[:space:]]+\\([[:alnum:]]+\\)" 2)))
-  )
--- doom.d/modules/lang/protobuf/packages.el
+++
@@ -1,4 +0,0 @@
-;; -*- no-byte-compile: t; -*-
-;;; lang/proto/packages.el
-
-(package! protobuf-mode)
