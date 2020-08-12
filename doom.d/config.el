fa4d92476660f262a1a35ba02260554a2fd333bc
sdvcrx <memory.silentvoyage@gmail.com>
2020-08-12 21:57:33 +0800

    emacs: configurate centaur tabs hide list

--- doom.d/config.el
+++ doom.d/config.el
@@ -98,6 +98,42 @@
    )
  )
 
+(after! centaur-tabs
+  (defun centaur-tabs-hide-tab (x)
+    "Do no to show buffer X in tabs."
+    (let ((name (format "%s" x)))
+      (or
+       ;; Current window is not dedicated window.
+       (window-dedicated-p (selected-window))
+
+       ;; Buffer name not match below blacklist.
+       (string-prefix-p "*epc" name)
+       (string-prefix-p "*helm" name)
+       (string-prefix-p "*Helm" name)
+       (string-prefix-p "*Compile-Log*" name)
+       (string-prefix-p "*lsp" name)
+       (string-prefix-p "*company" name)
+       (string-prefix-p "*Flycheck" name)
+       (string-prefix-p "*tramp" name)
+       (string-prefix-p " *Mini" name)
+       (string-prefix-p "*help" name)
+       (string-prefix-p "*straight" name)
+       (string-prefix-p " *temp" name)
+       (string-prefix-p "*Help" name)
+
+       (string-prefix-p "*Org" name)
+       (string-prefix-p "*org-roam" name)
+       (string-prefix-p "*Messages" name)
+       (string-prefix-p "*scratch" name)
+       (string-prefix-p "*doom" name)
+       (string-prefix-p "*tide-server" name)
+       (string-prefix-p "*vls" name)
+
+       ;; Is not magit buffer.
+       (and (string-prefix-p "magit" name)
+            (not (file-name-extension name)))
+       )))
+  )
 ;;
 ;; language spec keybindings
 ;;
