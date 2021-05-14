a84729f140f729d783b097b284d461f2770db841
sdvcrx <memory.silentvoyage@gmail.com>
2021-05-14 21:56:43 +0800

    emacs: disable ivy +prescient

--- doom.d/init.el
+++ doom.d/init.el
@@ -24,7 +24,7 @@
        ;;helm              ; the *other* search engine for love and life
        ;;ido               ; the other *other* search engine...
        (ivy               ; a search engine for love and life
-        +prescient
+        ;;+prescient
         +icons
         +fuzzy)
 
