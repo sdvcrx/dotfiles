579d2ada14ed294f1ee001581b92070eeb4c0283
sdvcrx <memory.silentvoyage@gmail.com>
2021-06-27 22:42:33 +0800

    emacs: enable python +pyright

--- doom.d/init.el
+++ doom.d/init.el
@@ -172,6 +172,8 @@
        ;;plantuml          ; diagrams for confusing people more
        ;;purescript        ; javascript, but functional
        (python            ; beautiful is better than ugly
+        +lsp
+        +pyright
         +ipython)
        ;;qt                ; the 'cutest' gui framework ever
        racket            ; a DSL for DSLs
