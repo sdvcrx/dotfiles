612f3562a63e3de928c2e4f50393d487426dac3f
sdvcrx <memory.silentvoyage@gmail.com>
2022-08-23 12:16:40 +0800

    nvim: update key mappings

--- nvchad/mappings.lua
+++ nvchad/mappings.lua
@@ -1,6 +1,12 @@
 local M = {}
 
 M.user = {
+  i = {
+    -- emacs style key mappings
+    ["<C-a>"] = { "<ESC>^i", "beginning of line" },
+    ["<C-b>"] = { "<Left>", "move left" },
+    ["<C-f>"] = { "<Right>", "move right" },
+  },
   n = {
     [";"] = { ":", "Command" },
 
@@ -10,24 +16,86 @@ M.user = {
       end,
       "close buffer",
     },
+
     ["<leader>bb"] = {
       "<cmd> Telescope buffers <CR>", "close buffer",
     },
+
     ["<leader><leader>"] = { "<cmd> Telescope find_files follow=true no_ignore=true hidden=true <CR>", "find all" },
 
-    ["<leader>fs"] = {
-      "<cmd> write <CR>", "save current buffer",
-    },
-    ["<leader>fS"] = {
-      "<cmd> wall <CR>", "save all files",
-    },
+    ["<leader>fs"] = { "<cmd> write <CR>", "save current buffer" },
+
+    ["<leader>fS"] = { "<cmd> wall <CR>", "save all files" },
 
     ["<leader>op"] = {
       "<cmd> NvimTreeToggle <CR>", "toggle nvimtree",
     },
 
+    ["<leader>/"] = {
+      "<cmd> Telescope live_grep <CR>", "Search keyword in project",
+    },
     ["<leader>sp"] = {
       "<cmd> Telescope live_grep <CR>", "Search keyword in project",
+    },
+
+    ["<leader>sS"] = {
+      "<cmd> Telescope grep_string <CR>", "Search keyword under cursor",
+    },
+
+    -- nvterm
+    ["<leader>ot"] = {
+      function()
+        require("nvterm.terminal").new "horizontal"
+      end,
+      "new horizontal term",
+    },
+
+    -- lsp
+    ["<leader>cr"] = {
+      function()
+        require("nvchad_ui.renamer").open()
+      end,
+      "lsp rename",
+    },
+    ["<leader>cd"] = {
+      function()
+        vim.lsp.buf.definition()
+      end,
+      "lsp references",
+    },
+    ["<leader>cD"] = {
+      function()
+        vim.lsp.buf.references()
+      end,
+      "lsp references",
+    },
+    ["<leader>ci"] = {
+      function()
+        vim.lsp.buf.implementation()
+      end,
+      "lsp implementation",
+    },
+  },
+}
+
+-- gcc to toggle comment
+M.comment = {
+  plugin = true,
+
+  -- toggle comment in both modes
+  n = {
+    ["gcc"] = {
+      function()
+        require("Comment.api").toggle.linewise.current()
+      end,
+      "toggle comment",
+    },
+  },
+
+  v = {
+    ["gcc"] = {
+      "<ESC><cmd>lua require('Comment.api').toggle.linewise(vim.fn.visualmode())<CR>",
+      "toggle comment",
     },
   },
 }
