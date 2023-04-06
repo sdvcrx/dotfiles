7ae47ede1fba1ee10fffff7625aeae4341a0f557
sdvcrx <memory.silentvoyage@gmail.com>
2023-04-06 22:01:31 +0800

    nvim: fix null-ls config

--- nvchad/configs/null-ls.lua
+++ nvchad/configs/null-ls.lua
@@ -7,19 +7,18 @@ end
 local b = null_ls.builtins
 
 local sources = {
-
   -- webdev stuff
-  b.formatting.deno_fmt, -- choosed deno for ts/js files cuz its very fast!
-  b.formatting.prettier.with { filetypes = { "html", "markdown", "css" } }, -- so prettier works only on these filetypes
+  -- b.formatting.deno_fmt, -- choosed deno for ts/js files cuz its very fast!
+  -- b.formatting.prettier.with({ filetypes = { "html", "markdown", "css" } }), -- so prettier works only on these filetypes
 
   -- Lua
   b.formatting.stylua,
 
-  -- cpp
-  b.formatting.clang_format,
+  -- Go
+  b.formatting.gofmt,
+  b.formatting.goimports,
 }
 
-null_ls.setup {
-  debug = true,
+null_ls.setup({
   sources = sources,
-}
+})
