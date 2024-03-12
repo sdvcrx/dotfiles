22d75d18a79e13efed836bd7aea3f6e4e14f51a0
sdvcrx <memory.silentvoyage@gmail.com>
2024-03-12 17:56:00 +0800

    nvim: add go.nvim

--- nvchad/plugins.lua
+++ nvchad/plugins.lua
@@ -97,14 +97,24 @@ return {
     end
   },
 
-  -- ["ray-x/go.nvim"] = {
-  --   disable = true,
-  --   ft = "go",
-  --   opt = true,
-  --   config = function ()
-  --     require('go').setup()
-  --   end,
-  -- },
+  {
+    "ray-x/go.nvim",
+    ft = {"go", "gomod"},
+    event = "CmdlineEnter",
+    config = function()
+      require("go").setup()
+
+      local format_sync_grp = vim.api.nvim_create_augroup("GoFormat", {})
+      vim.api.nvim_create_autocmd("BufWritePre", {
+        pattern = "*.go",
+        callback = function()
+          require('go.format').gofmt()
+        end,
+        group = format_sync_grp,
+      })
+    end,
+  },
+
   -- ["windwp/nvim-ts-autotag"] = {
   --   ft = { "vue", "html", "typescript", "javascript", "tsx", "jsx" },
   --   config = function ()
